31356143d7d72228df77bc57e1f1ca032856ee02
nk-on <2nkoz@proton.me>
2024-01-06 21:24:43 +0400

    feat:implementing text colapse behaviour after user clicks on minus sign

---
+++ src/script.js
@@ -0,0 +1,9 @@
+const signIcons = document.querySelectorAll(".sign-icon");
+const hiddenTextElements = document.querySelectorAll(".text");
+signIcons.forEach(
+    (icon,index) => {
+        icon.addEventListener("click", () => {
+            hiddenTextElements[index].classList.remove("hidden")
+        });
+    }
+);
