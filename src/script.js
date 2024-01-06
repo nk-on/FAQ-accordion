5eee1c90ea3d837e56fa96d77a87f64150038751
nk-on <2nkoz@proton.me>
2024-01-06 21:47:24 +0400

    feat:adding hiding behaviour when user clicks on plus sign

--- src/script.js
+++ src/script.js
@@ -1,10 +1,16 @@
 const signIcons = document.querySelectorAll(".sign-icon");
 const hiddenTextElements = document.querySelectorAll(".text");
 signIcons.forEach(
-    (icon,index) => {
+    (icon, index) => {
         icon.addEventListener("click", () => {
-            hiddenTextElements[index].classList.remove("hidden");
-            icon.src = "assets/images/icon-plus.svg";
+            if (!icon.classList.contains("plus")) {
+                hiddenTextElements[index].classList.remove("hidden");
+                icon.src = "assets/images/icon-plus.svg";
+                icon.classList.add("plus");
+            }else{
+                icon.src = "assets/images/icon-minus.svg";
+                hiddenTextElements[index].classList.add("hidden");
+            }
         });
     }
 );
