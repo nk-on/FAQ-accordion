23c60889ebc00ba9326d2d2a010afc0bc143684e
nk-on <2nkoz@proton.me>
2024-01-06 21:27:50 +0400

    when user clicks minus sign it turns to plus sign

--- src/script.js
+++ src/script.js
@@ -3,7 +3,8 @@ const hiddenTextElements = document.querySelectorAll(".text");
 signIcons.forEach(
     (icon,index) => {
         icon.addEventListener("click", () => {
-            hiddenTextElements[index].classList.remove("hidden")
+            hiddenTextElements[index].classList.remove("hidden");
+            icon.src = "assets/images/icon-plus.svg";
         });
     }
 );
